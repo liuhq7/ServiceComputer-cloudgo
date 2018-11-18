6d52bf4142962565a96ad85c498f74855f4a58e2
liuhq7 <44065019+liuhq7@users.noreply.github.com>
2018-11-18 21:47:29 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"cloudgo/service"
+	flag "github.com/pflag"
+)
+
+const (
+	PORT string = "9000"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = PORT
+	}
+
+	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
+	flag.Parse()
+	if len(*pPort) != 0 {
+		port = *pPort
+	}
+
+	server := service.NewServer()
+	server.Run(":" + port)
+}
